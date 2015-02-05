caf053d863546b651f5157b56138b6425ab9b704
Juan Pedro Mermoz <jpmermoz@hotmail.com>
2015-02-05 11:05:51 -0300

    Set table name to exception_importance class

--- app/models/exception_logger/exception_importance.rb
+++ app/models/exception_logger/exception_importance.rb
@@ -1,5 +1,7 @@
 module ExceptionLogger
 	class ExceptionImportance < ActiveRecord::Base
+		self.table_name = "exception_importances"
+
 		validates :name, presence: true, uniqueness: true
 
 	  has_many :logged_exceptions, class_name: "LoggedException"
